80e34abcaf5ee849bd06760d272b74fd7a05f871
Inshalmunaf <inshalmunaf@gmail.com>
2023-08-17 21:51:42 +0500

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,19 +9,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
- 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('My first app on flutter'),
-          ),
-        ),
-      ),
+      home:HomePage(),
     );
   }
 }
-
-
